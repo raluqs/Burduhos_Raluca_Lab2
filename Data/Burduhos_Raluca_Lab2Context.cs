7bb8e31a606f426ac28cbe4f978b9bc440df4af8
Raluca <ralucaburduhos@gmail.com>
2023-11-06 20:24:14 +0200

    Laborator5:Am creat LibraryIdentity sarcina 15

--- Data/Burduhos_Raluca_Lab2Context.cs
+++ Data/Burduhos_Raluca_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Burduhos_Raluca_Lab2.Data
         public DbSet<Burduhos_Raluca_Lab2.Models.Author>? Author { get; set; }
 
         public DbSet<Burduhos_Raluca_Lab2.Models.Category>? Category { get; set; }
+
+        public DbSet<Burduhos_Raluca_Lab2.Models.Member>? Member { get; set; }
+
+        public DbSet<Burduhos_Raluca_Lab2.Models.Borrowing>? Borrowing { get; set; }
     }
 }
