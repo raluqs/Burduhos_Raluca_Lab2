2d7edcd0608ad108743d50bce298106fba22f52e
Raluca <ralucaburduhos@gmail.com>
2023-11-13 18:39:36 +0200

    Sarcina 2 de la laborator 5 este rezolvata

--- Pages/Borrowings/Details.cshtml.cs
+++ Pages/Borrowings/Details.cshtml.cs
@@ -28,7 +28,11 @@ namespace Burduhos_Raluca_Lab2.Pages.Borrowings
                 return NotFound();
             }
 
-            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+            var borrowing = await _context.Borrowing
+                .Include(b => b.Member)
+                .Include(b => b.Book)
+                .FirstOrDefaultAsync(m => m.ID == id);
+
             if (borrowing == null)
             {
                 return NotFound();
